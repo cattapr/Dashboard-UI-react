ace063a3b2c4226e117ab30ede8518d401b271e0
unknown <catalinapradilla@hotmail.com>
2018-05-28 17:41:34 +0200

    clock working

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class Clock extends React.Component {
-	render(){
-		return (
-			<p> This is a clock </p>
-		)
-	}
+    state = {
+        date: new Date()
+      }
+    
+      componentDidMount() {
+        setInterval(
+          () => this.update(),
+          1000
+        );
+      }
+    
+      update = () => {
+        this.setState({
+          date: new Date()
+        });
+      }
+
+    render() {
+        return (
+            <h1> { this.state.date.toLocaleTimeString() }</h1>
+        )
+    }
 }
 
 export default Clock;
