d0a63d6caed9d7269b02c3154df31460b577ab3f
unknown <catalinapradilla@hotmail.com>
2018-06-01 15:09:45 +0200

    Updates

--- src/components/ChartTitle.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-const ChartTitle = (props) => {
-		return(<h4 className={props.chartTitleClass}>{props.chartTitle}</h4>)
-}
-
-export default ChartTitle;
-
--- src/components/PinnedPost.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-const PinnedPost = (props) => {
-		return(<h4 className={props.chartTitleClass}><i class="fas fa-thumbtack fa-1x"></i> {props.chartTitle}</h4>)
-}
-
-export default PinnedPost;
